3a98e8ca702f1531612876776e5950df3f6082be
knyatom <knyatom@daum.net>
2023-08-08 17:08:18 +0900

    spring first

---
+++ src/main/java/com/example/sixproject/SixprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sixproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sixproject/SixprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sixproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
